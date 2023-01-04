7490396c8260761d2298a39c0e590df325293e6f
godfreyyeung <godfreyyeung@gmail.com>
2023-01-04 15:15:45 -0500

    1. Basic Prop Drilling

--- src/NameInput.js
+++ src/NameInput.js
@@ -1,8 +1,28 @@
-export const NameInput = ({ isDisabled = false }) => {
+import { useState } from "react";
+
+export const NameInput = ({
+    isDisabled = false,
+    numFilled,
+    setNumFilled
+  }) => {
+
+  const [alreadyFilled, setAlreadyFilled] = useState(false);
+
+  const updateFilled = () => {
+    if(!alreadyFilled) {
+      setAlreadyFilled(true);
+      setNumFilled(numFilled + 1);
+    }
+  }
+
   return (
     <div>
+      Question 1/3 <br />
       Enter Your Name:
-      <input disabled={isDisabled ? "disabled" : ""}/>
+      <input
+       disabled={isDisabled ? "disabled" : ""}
+       onChange={updateFilled}
+      />
     </div>
   )
 }
