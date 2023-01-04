6986be2f93ba4d6d63ebe2d6e4342722c7cab9af
godfreyyeung <godfreyyeung@gmail.com>
2023-01-04 14:28:57 -0500

    Add extra input

--- src/Conditions.js
+++ src/Conditions.js
@@ -3,6 +3,9 @@ export const Conditions = ({ isDisabled = false }) => {
     <div>
       Check box to accept condition 1 
       <input disabled={isDisabled ? "disabled" : ""} type="checkbox" />
+      <br />
+      Check box to accept condition 2
+      <input disabled={isDisabled ? "disabled" : ""} type="checkbox" />
     </div>
   )
 }
