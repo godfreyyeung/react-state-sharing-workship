1413014ff44a15bc0121ed71ddf619e84aed569c
Godfrey Yeung <godfreyyeung@gmail.com>
2023-02-09 12:49:19 -0500

    component hoisting/decomposition

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 import './App.css';
-import { useState, useContext, createContext } from "react";
+import { useState } from "react";
 import { FormSection } from './FormSection';
 import { formReducer } from './formReducer';
 
-export const formContext = createContext({
-  isSubmitting: false,
-  numFilled: 0
-})
+const NameInput = ({numFilled, setNumFilled, isSubmitting, name, setName}) => {
+  const [alreadyFilled, setAlreadyFilled] = useState(false);
+
+  const updateFilled = () => {
+    if(!alreadyFilled) {
+      setAlreadyFilled(true);
+      setNumFilled(numFilled + 1);
+    }
+  }
+
+  return (
+    <div>
+      <input
+        disabled={isSubmitting ? "disabled" : ""}
+        onChange={(evt) => {
+          setName(evt.target.value);
+          updateFilled();
+        }}
+        value={name}
+        key="editor1"
+      />
+   </div>
+  )
+}
 
 function App() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [numFilled, setNumFilled] = useState(0);
+  const [name, setName] = useState("");
 
-
-  const [form, setForm] = useState({
-    isSubmitting: false,
-    numFilled: 0
-  })
 
   const onSubmit = () => {
-    // formDispatch({ type: 'toggleIsSubmitting' });
 
-    // setTimeout(() => {
-    //   formDispatch({ type: 'toggleIsSubmitting' });
-    // }, 1000);
   }
- 
+
   return (
     <div className="App">
       Form 1 
-      <formContext.Provider value={{...form, setForm}}>
-        Questions Filled out: {form.numFilled} / 3 
+        Questions Filled out: {numFilled} / 3 
         <br />
         <button onClick={onSubmit}> Submit </button>
         <br />
-        <FormSection />
-      </formContext.Provider>
+        <FormSection
+          NameInput={<NameInput numFilled={numFilled} setNumFilled={setNumFilled} isSubmitting={isSubmitting} name={name} setName={setName} />}
+        />
     </div>
   );
 }
--- src/FormSection.js
+++ src/FormSection.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import { NameInput } from './NameInput';
 import { Conditions } from './Conditions';
 
 export const FormSection = ({
-  form,
-  formDispatch
+  NameInput
 }) => {
   return (
     <div>
-      <NameInput />
+      Q1
+      {NameInput}
     </div>
   )
 }
